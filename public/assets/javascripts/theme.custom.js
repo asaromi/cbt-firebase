522b0e04e1f9562cd4785790107d6d37a24d9667
asaromi <a.sabililromi@gmail.com>
2020-07-13 12:27:52 +0700

    Pages : add users page for Admin

--- public/assets/javascripts/theme.custom.js
+++ public/assets/javascripts/theme.custom.js
@@ -4,31 +4,122 @@ const auth = firebase.auth();
 const stack_bar_top = {"dir1": "down", "dir2": "right", "push": "top", "spacing1": 0, "spacing2": 0};
 const pwd = window.location.pathname;
 
-if (pwd == "/home.html" || pwd == "/soal.html") {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if(user){
-            alert("success login\nEmail : "+user.email);
-            $('#preloader').attr('hidden',true);
-            $('.profile-info').find('.name').html(user.displayName);
-            
-            $('.profile-info').attr('data-lock-name', user.displayName);
-            $('.profile-info').attr('data-lock-email', user.email);
-            $('figure.profile-picture img').attr('alt', user.displayName);
-            
-            $('figure.profile-picture img').attr('src', '/assets/images/!logged-user.jpg');
+var users;
 
-            if(JSON.parse(sessionStorage.user)["admin"])
-                $('.profile-info').find('.role').html("admin");
+    auth.onAuthStateChanged(function(user) {
+        if(user){
+            if(!user.displayName && sessionStorage.name){
+                console.log("Berhasil masuk sini");
+                user.updateProfile({
+                    displayName : sessionStorage.name,
+                }).then(function(){
+                    user.sendEmailVerification().then(function(){
+                        alert("Silahkan verifikasi akun melalui email dan konfirmasi ke wa.me/...");
+                        auth.signOut();
+                    }).catch(function(error){
+                        alert(error.message);
+                    });;
+                }).catch(function(error){
+                    alert(error.message);
+                });
+            }
+
+            else {
+                if(JSON.parse(sessionStorage.user)["admin"]) {
+                    $('.profile-info').find('.role').html("admin");
+                } else {
+                    $('.nav-main').find('.admin').css('display', 'none');
+                }
+
+                console.log("Gagal Masuk sini");
+                
+                if(pwd !== '/users.html' && pwd !== "/quiz.html")
+                    $('#preloader').attr('hidden',true);
+
+                $('.profile-info').find('.name').html(user.displayName);
+                $('.profile-info').attr('data-lock-name', user.displayName);
+                $('.profile-info').attr('data-lock-email', user.email);
+
+                $('.profile-user').find('.name').html(user.displayName);
+                $('.profile-user').find('.email').html(user.email);
+
+                $('figure.profile-picture img').attr('alt', user.displayName);
+                $('figure.profile-picture img').attr('src', '/assets/images/!logged-user.jpg');
+            }
             
         } else{
             console.log('no user');
             window.location.href = "login.html";
         }
     });
-}
 
 /* FOR admin.html */
-if(pwd == "/500.html"){
+if(pwd == "/users.html"){
+    db.ref('users').orderByKey().on('value', function(shot){
+        users = snapshotToArray(shot);
+        if(users){
+            $('#myTable').DataTable({
+                data : users,
+                bDestroy : true,
+                columns : [
+                { title : "Id" },
+                { title : "Name" },
+                { title : "Email" },
+                { title : "Verif" },
+                { title : "Action", width : "28%" },
+                ],
+                columnDefs : [
+                { className: "text-center", targets: [0,3,4] },
+                ]
+            })
+            $('#preloader').attr('hidden', true);
+        }
+
+        $('td').find('.btn-success').on('click', function(){
+            console.log("klik verifikasi user");
+            var btn = $(this);
+            btn.attr('disabled', true);
+            var id = btn.attr('user');
+            db.ref("users").child(id).update({
+                verified : true,
+            }).then( function(){ alert('User sudah terverifikasi');} )
+            .catch(function(error){ alert(error.message) });
+        });
+
+        $('td').find('.btn-danger').on('click', function(){
+            console.log("klik matikan user");
+            var btn = $(this);
+            btn.attr('disabled', true);
+            var id = btn.attr('user');
+            db.ref("users").child(id).update({
+                verified : false,
+            }).then( function(){ alert('User sudah terverifikasi');} )
+            .catch(function(error){ alert(error.message) });
+        });
+
+        $('td').find('.btn-primary').on('click', function(){
+            console.log("klik jadikan admin");
+            var btn = $(this);
+            btn.attr('disabled', true);
+            var id = btn.attr('user');
+            db.ref("users").child(id).update({
+                verified : true,
+            }).then( function(){ alert('Role Admin telah ditambahkan pada User');} )
+            .catch(function(error){ alert(error.message) });
+        });
+
+        $('td').find('.btn-danger').on('click', function(){
+            console.log("klik matikan admin");
+            var btn = $(this);
+            btn.attr('disabled', true);
+            var id = btn.attr('user');
+            db.ref("users").child(id).update({
+                verified : false,
+            }).then( function(){ alert('Role Admin telah dihapus dari User');} )
+            .catch(function(error){ alert(error.message) });
+        });
+    });
+
     if(!sessionStorage.user){
         window.location.href = "/404.html";
     } else {
@@ -38,6 +129,37 @@ if(pwd == "/500.html"){
             window.location.href = "/404.html";
     }
 }
+
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function(childSnapshot) {
+        var item = [];
+        item.push(childSnapshot.key);
+        item.push(childSnapshot.val()["name"]);
+        item.push(childSnapshot.val()["email"]);
+        var action;
+        if(!childSnapshot.val()["verified"]){
+            item.push('<td class="verified text-center"><i class="fa fa-times" style="color : #d2322d"></i></td>');
+            action = '<td class="text-center"><button user="'+childSnapshot.key+'" class="btn btn-success"><i class="fa fa-check"></i> Aktifkan</button>';
+        } else{
+            item.push('<td class="verified text-center"><i class="fa fa-check" style="color : #47a447"></i></td>');
+            action = '<td class="text-center"><button user="'+childSnapshot.key+'" class="btn btn-danger"><i class="fa fa-times"></i> Matikan</button>';
+        }
+
+        if(!childSnapshot.val()["admin"])
+            action = action+' <button user="'+childSnapshot.key+'" class="btn btn-primary"><i class="fa fa-key"></i> Jadikan Admin</button></td>';
+        else
+            action = action+' <button user="'+childSnapshot.key+'" class="btn btn-warning"><i class="fa fa-key"></i> Matikan Admin</button></td>';
+
+        item.push(action);
+
+        returnArr.push(item);
+    });
+
+    return returnArr;
+};
+    
 
 function getEmail() {
     var email = $("input[type='email']").val();
@@ -92,7 +214,7 @@ function resetPassword() {
     }).catch(function(error) {
         var notice = new PNotify({
             title: 'ERROR',
-            text: 'Email ',
+            text: error.message,
             type: 'error',
             addclass: 'stack-bar-top',
             stack: stack_bar_top,
@@ -105,3 +227,16 @@ function resetPassword() {
     });
     // window.location.href = "login.html"
 }
+
+// function check() {
+//     auth.languageCode = 'id';
+//     var email = getEmail();
+//     var actionCodeSettings = {
+//         url : 'https://asaromi-dev.web.app/?email='+email,
+//         handleCodeInApp : true,
+//     }
+
+//     auth.sendSignInLinkToEmail(email, actionCodeSettings).then(function(){
+//         alert('Verification Link has been sent to your email');
+//     });
+// }
